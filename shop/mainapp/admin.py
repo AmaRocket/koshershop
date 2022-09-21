746b8529ec88819b3bb03b29405d8cfce883bb2b
AmaRocket <alexeverdie999@gmail.com>
2022-09-21 21:37:32 +0200

    ADD CATEGORY AND STATIC IMAGE WAY NEED TO CHANGE IT TO S3 ETC

--- shop/mainapp/admin.py
+++ shop/mainapp/admin.py
@@ -1,3 +1,31 @@
+from django import forms
 from django.contrib import admin
+from django.forms import ModelChoiceField
 
-# Register your models here.
+from .models import *
+
+
+class CakeAdmin(admin.ModelAdmin):
+
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        if db_field.name == 'category':
+            return ModelChoiceField(Category.objects.filter(slug='Cake'))
+        return super().formfield_for_foreignkey(db_field, request, **kwargs)
+
+
+class BaketyAdmin(admin.ModelAdmin):
+
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        if db_field.name == 'category':
+            return ModelChoiceField(Category.objects.filter(slug='Bekery'))
+        return super().formfield_for_foreignkey(db_field, request, **kwargs)
+
+
+admin.site.register(Category)
+
+admin.site.register(Bakery, BaketyAdmin)
+admin.site.register(Cake, CakeAdmin)
+
+admin.site.register(CartProduct)
+admin.site.register(Cart)
+admin.site.register(Customer)
--- shop/mainapp/models.py
+++ shop/mainapp/models.py
@@ -1,4 +1,5 @@
 from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
 from django.db import models
 from django.contrib.auth import get_user_model
 
@@ -23,6 +24,9 @@ class Category(models.Model):
 
 
 class Product(models.Model):
+    class Meta:
+        abstract = True
+
     category = models.ForeignKey(Category, verbose_name="Category", on_delete=models.CASCADE)
     title = models.CharField(max_length=255, verbose_name="Product name")
     slug = models.SlugField(unique=True)
@@ -37,12 +41,31 @@ class Product(models.Model):
 class CartProduct(models.Model):
     user = models.ForeignKey('Customer', verbose_name="Customer", on_delete=models.CASCADE)
     cart = models.ForeignKey("Cart", verbose_name="Cart", on_delete=models.CASCADE, related_name="related_products")
-    product = models.ForeignKey(Product, verbose_name="Product", on_delete=models.CASCADE)
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey('content_type', 'object_id')
     quantity = models.PositiveIntegerField(default=1)
     final_price = models.DecimalField(max_digits=9, decimal_places=2, verbose_name="Total price")
 
     def __str__(self):
-        return f"Cart Item: {self.product.title}"
+        return f"Cart Item: {self.title}"
+
+
+class Bakery(Product):
+    ingredients = models.TextField(verbose_name="Ingredients", null=True)
+    allergens = models.CharField(max_length=255, verbose_name='Allergens')
+
+    def __str__(self):
+        return f"{self.category.name} : {self.title}"
+
+
+class Cake(Product):
+    ingredients = models.TextField(verbose_name="Ingredients", null=True)
+    allergens = models.CharField(max_length=255, verbose_name='Allergens')
+    weight = models.CharField(max_length=255, verbose_name='Weight')
+
+    def __str__(self):
+        return f"{self.category.name} : {self.title}"
 
 
 class Cart(models.Model):
@@ -59,15 +82,8 @@ class Customer(models.Model):
     user = models.ForeignKey(User, verbose_name="User", on_delete=models.CASCADE)
     phone = models.CharField(max_length=20, verbose_name="Phone number")
     adress = models.CharField(max_length=255, verbose_name="Adress")
+    email = models.EmailField(max_length=255, verbose_name='Email')
+    birthday = models.DateField(help_text = "Customer birthdate", verbose_name='Birthday')
 
     def __str__(self):
         return f"User: {self.user.first_name} {self.user.last_name}"
-
-
-class Specification(models.Model):
-    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
-    object_id = models.PositiveIntegerField()
-    name = models.CharField(max_length=255, verbose_name="Product name for specifications")
-
-    def __str__(self):
-        return f"Specifications for product: {self.name}"
